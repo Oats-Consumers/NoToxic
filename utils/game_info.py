1638d66adf7732a987ce3a30429b0094fcd84207
imrun <turganovimrun@gmail.com>
2025-05-14 04:19:34 +0200

    updated backend

--- utils/game_info.py
+++ utils/game_info.py
@@ -28,7 +28,18 @@ def fetch_recent_matches(account_id):
     url = f"https://api.opendota.com/api/players/{account_id}/recentMatches?api_key={OPENDOTA_API_KEY}"
     try:
         response = requests.get(url)
-        print(response.json())
+        #print(response.json())
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching recent matches: {e}")
+        return None
+
+def fetch_player(account_id):
+    url = f"https://api.opendota.com/api/players/{account_id}?api_key={OPENDOTA_API_KEY}"
+    try:
+        response = requests.get(url)
+        #print(response.json())
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
